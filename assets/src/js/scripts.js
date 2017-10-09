192a0e68164b0932d3ba88770515d773dcbd1a36
Wladimir1982 <wsidorowskiy@gmail.com>
2017-10-09 19:42:15 +0300

    animation

--- assets/src/js/scripts.js
+++ assets/src/js/scripts.js
@@ -166,8 +166,47 @@ $('.works__slider').slick({
 
     // Плавный скрол по якорям конец
 
+//    Скрипт анимации ачало
+
+    new WOW().init();
+
+    //    Скрипт анимации конец
 
 
-        $(document).foundation();
+
+    // Cкрипт для фиксации меню начало
+    jQuery(document).ready(function($) {
+        var
+
+            $window = $(window), // Основное окно
+
+            $target = $(".top-bar"), // Блок, который нужно фиксировать при прокрутке
+
+            $h = $target.offset().top; // Определяем координаты верха нужного блока (например, с навигацией или виджетом, который надо фиксировать)
+
+        $window.on('scroll', function() {
+
+            // Как далеко вниз прокрутили страницу
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+            // Если прокрутили скролл ниже макушки нужного блока, включаем ему фиксацию
+            if (scrollTop > $h) {
+
+                $target.addClass("sheensay_fixed");
+
+                // Иначе возвращаем всё назад
+            } else {
+
+                $target.removeClass("sheensay_fixed");
+            }
+        });
+
+    });
+
+// Cкрипт для фиксации меню конец
+
+
+
+    $(document).foundation();
 
 })(jQuery);
